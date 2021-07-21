e6b5896eb72808bdffb16ea43305f566d078e924
gglue <generalglue@gmail.com>
2021-07-21 05:43:48 -0400

    Added accompaying icons and added tmmrw's forecast

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,8 @@
 const searchBar = document.querySelector('form');
 const box = document.querySelector('.d-none');
 const infoBox = document.querySelector('.card-body');
-const picture = document.querySelector('.time');
-
+const picture = document.querySelector('.card-img-top');
+const weatherIcon = document.querySelector('.icon img');
 
 searchBar.addEventListener('submit', e => {
     e.preventDefault();
@@ -26,12 +26,22 @@ const updateUI = async (city) => {
     }
 }
 
+const updateUITmmrw = async (city) => {
+    const details = await getCity(city);
+    const weather = await getTommorowWeather(details.Key);
+    return {
+        details: details,
+        weather: weather
+    }
+}
+
 const updateInfo = (cityInfo) => {
     const info = cityInfo.details;
     const weather = cityInfo.weather;
 
     if (box.classList.contains('d-none')) {box.classList.remove('d-none')};
 
+    // Updates the information
     infoBox.innerHTML = `
     <div class = "card-title text-muted text-uppercase text-center details"> 
     <h5>${info.EnglishName}</h5> 
@@ -42,12 +52,84 @@ const updateInfo = (cityInfo) => {
     </div>
     <div class="my-1">${weather.LocalObservationDateTime}</div>
     </div>
-    <div class ="row text-center text-uppercase my-3">
+    <div class ="row text-center text-uppercase my-3 tmmrw">
         <div class = "col" style ="cursor: pointer;">
-            Yesterday
-        </div>
-        <div class = "col" style ="cursor: pointer;">
-            Tommorow
+        Tommorow ->
         </div>
     </div>`
+
+    // Add eventListener to the Tommorow Button
+    const button = document.querySelector('.tmmrw');
+
+    button.addEventListener('click', e => {
+        e.preventDefault();
+
+        // Trim the input to make sure it's readable
+        const city = info.EnglishName;
+    
+        // Use input to update the UI
+        updateUITmmrw(city).then(data  => updateInfoTmmrw(data))
+        .catch(err => console.log(err));
+    });
+
+    // Updates the time of day picture
+    let timeOfDay = null;
+    if (weather.IsDayTime){
+        timeOfDay = 'img/day.svg';
+    }
+    else {
+        timeOfDay = 'img/night.svg';
+    }
+    picture.setAttribute('src', timeOfDay);
+
+    // Updates the weather icon
+    const weatherIconSource = `img/icons/${weather.WeatherIcon}.svg`;
+    weatherIcon.setAttribute('src', weatherIconSource);
+
+};
+
+const updateInfoTmmrw = (cityInfo) => {
+    const info = cityInfo.details;
+    const weather = cityInfo.weather;
+
+    if (box.classList.contains('d-none')) {box.classList.remove('d-none')};
+
+    // Updates the information
+    infoBox.innerHTML = `
+    <div class = "card-title text-muted text-uppercase text-center details"> 
+    <h5>${info.EnglishName}</h5> 
+    <div class="my-3">${weather.Headline.Category}</div>
+    <div class="display-4 my-4">
+      <span>${Math.floor((weather.DailyForecasts[0].Temperature.Maximum.Value - 32) * (5/9))}</span>
+      <span>&deg;C</span>
+    </div>
+    <div class="my-1">${weather.Headline.EffectiveDate}</div>
+    </div>
+    <div class ="row text-center text-uppercase my-3 today">
+        <div class = "col" style ="cursor: pointer;">
+        <- Today
+        </div>
+    </div>`
+
+    // Add eventListener to the Tommorow Button
+    const button = document.querySelector('.today');
+
+    button.addEventListener('click', e => {
+        e.preventDefault();
+
+        // Trim the input to make sure it's readable
+        const city = info.EnglishName;
+    
+        // Use input to update the UI
+        updateUI(city).then(data  => updateInfo(data))
+        .catch(err => console.log(err));
+    });
+
+    // Use default picture of night for tommorow
+    picture.setAttribute('src', 'img/day.svg');
+
+    // Updates the weather icon
+    const weatherIconSource = `img/icons/${weather.DailyForecasts[0].Day.Icon}.svg`;
+    weatherIcon.setAttribute('src', weatherIconSource);
+
 }
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -10,7 +10,7 @@ const getCity = async (city) => {
     return data[0];
 };
 
-// This functio nhelps get the weather information based on the city
+// This function helps get the weather information based on the city
 const getWeather = async (cityID) => {
     const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${cityID}?apikey=${key}`;
@@ -19,3 +19,13 @@ const getWeather = async (cityID) => {
 
     return data[0];
 };
+
+// This function helps get the weather information based on the city ahead
+const getTommorowWeather = async (cityID) => {
+    const base = 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/';
+    const query = `${cityID}?apikey=${key}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data;
+}
